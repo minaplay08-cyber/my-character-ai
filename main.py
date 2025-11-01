15af29bf4ccf1f8abd6b370866596ee61aa3ddfe
minaplay08-cyber <minaplay08@gmail.com>
2025-11-01 10:53:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -109,4 +109,5 @@ HTML_TEMPLATE = '''
 '''
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
